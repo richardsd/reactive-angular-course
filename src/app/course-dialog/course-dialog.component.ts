17ddb07f7318b28d6e7092dc853579471f3acc12
Ricardo Dias <ricardo.dias@hyland.com>
2022-12-27 15:33:55 +0000

    add local error handling

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -5,13 +5,17 @@ import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import * as moment from 'moment';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css'],
     providers: [
-        LoadingService
+        LoadingService,
+        MessagesService
     ]
 })
 export class CourseDialogComponent implements AfterViewInit {
@@ -25,7 +29,8 @@ export class CourseDialogComponent implements AfterViewInit {
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
         private coursesService: CoursesService,
-        private loadingService: LoadingService
+        private loadingService: LoadingService,
+        private messagesService: MessagesService
     ) {
 
         this.course = course;
@@ -46,7 +51,15 @@ export class CourseDialogComponent implements AfterViewInit {
     save() {
       const changes = this.form.value;
 
-      const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes);
+      const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
+        .pipe(
+            catchError(error => {
+                const message = 'Could not save the course';
+                console.log(message, error);
+                this.messagesService.showErrors(message);
+                return throwError(error);
+            }),
+        );
 
       this.loadingService.showLoaderUntilCompleted(saveCourse$)
         .subscribe((val) => {
