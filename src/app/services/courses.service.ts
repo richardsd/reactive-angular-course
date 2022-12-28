c4603128743e5009138f63d757391b81112bab43
Ricardo Dias <ricardo.dias@hyland.com>
2022-12-28 14:40:02 +0000

    use master detail ui pattern

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Course } from "../model/course";
 import { map, shareReplay } from "rxjs/operators";
+import { Lesson } from "../model/lesson";
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,19 @@ export class CoursesService {
     return this.http.put(`/api/courses/${courseId}`, changes)
       .pipe(
         shareReplay() // prevent from doing multiple updates
-      )
+      );
+  }
+
+  searchLessons(search: string): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', { 
+      params: {
+        filter: search,
+        pageSize: 100,
+      }
+     })
+      .pipe(
+        map(res => res['payload']),
+        shareReplay(),
+      );
   }
 }
